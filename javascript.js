454994f04b7725319315c0bc429c316415578fc9
julieg18 <julieheartcrochet@gmail.com>
2019-03-02 11:28:59 -0600

    added functions for decrement/increment btns

--- javascript.js
+++ javascript.js
@@ -1,4 +1,29 @@
 $(document).ready(function () {
-  
+  let breakLength = 5;
+  let sessionLength = 25;
+  $("#break-decrement").click(function() { 
+    if (breakLength > 0) {
+      breakLength--;
+      $("#break-length").html("<p>" + breakLength + "</p>");
+    }
+  });
+  $("#break-increment").click(function() { 
+    if (breakLength < 60) {
+      breakLength++;
+      $("#break-length").html("<p>" + breakLength + "</p>");
+    }
+  });
+  $("#session-decrement").click(function() { 
+    if (sessionLength > 0) {
+      sessionLength--;
+      $("#session-length").html("<p>"+ sessionLength +"</p>");
+    }
+  });
+  $("#session-increment").click(function() {
+    if (sessionLength < 60) {
+      sessionLength++;
+      $("#session-length").html("<p>"+ sessionLength + "</p>");
+    }
+  })
 });
 
